4dfdf05b00a29c9ef8aefc3c3ffb585c6d3b2525
noahhyden <noah@klarum.com>
2026-06-15 21:22:15 +0200

    Scratch

---
+++ engine/main.py
@@ -0,0 +1,46 @@
+import numpy as np 
+
+## Identifiera strömmarna
+
+
+
+## Definiera in-stream and out-stream
+## Define System Boundaries
+
+## Mixer, Separator, Total
+
+# Core math:
+## in - out + generated - consumed = 0
+
+# Unknowns
+
+A = [0,0,0,0,
+     0,0,0,0,
+     0,0,0,0,
+     0,0,0,-1,
+     0,0,0,1]
+
+
+# Species are atomslag
+
+def textInput():
+    answer = 0 
+    if input == "":
+        print("try again")
+    else:
+        answer = input()
+
+    return answer
+
+def splitter():
+    # out = 0.7
+    # back in = 0.3
+    return 0
+
+def main():
+    return 0
+
+
+if __name__ == "__main__":
+    main()
+
